b95cca124bf9b2e7afcc61cc983611ec61f313ea
idiot2222 <pky2892@gmail.com>
2022-08-06 07:04:05 +0900

    project init

---
+++ src/test/java/me/bogeun/yajalal/YajalalApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bogeun.yajalal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YajalalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
