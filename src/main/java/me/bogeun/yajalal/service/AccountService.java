3d584202041c8383f15792c7a802936ec1917bac
idiot2222 <pky2892@gmail.com>
2022-08-11 03:10:14 +0900

    add SecurityConfig
    패스워드 인코더 추가

--- src/main/java/me/bogeun/yajalal/service/AccountService.java
+++ src/main/java/me/bogeun/yajalal/service/AccountService.java
@@ -5,6 +5,7 @@ import me.bogeun.yajalal.entity.Account;
 import me.bogeun.yajalal.mapper.AccountMapper;
 import me.bogeun.yajalal.payload.AccountJoinDto;
 import me.bogeun.yajalal.repository.AccountRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -12,9 +13,13 @@ import org.springframework.stereotype.Service;
 public class AccountService {
 
     private final AccountRepository accountRepository;
+
     private final AccountMapper accountMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public Account joinNewAccount(AccountJoinDto joinDto) {
+        joinDto.setPassword(passwordEncoder.encode(joinDto.getPassword()));
+
         Account account = accountMapper.joinDtoToEntity(joinDto);
 
         return accountRepository.save(account);
