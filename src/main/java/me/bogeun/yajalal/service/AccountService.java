a2c93da0248c64d32fb9d62f37e336db27d4c4ae
idiot2222 <pky2892@gmail.com>
2022-08-26 04:48:46 +0900

    modify AccountService
    AccountService가 구현하고 있던 UserDetailsService를 빼고 따로 AccountDetailsService를 만듦

--- src/main/java/me/bogeun/yajalal/service/AccountService.java
+++ src/main/java/me/bogeun/yajalal/service/AccountService.java
@@ -2,59 +2,37 @@ package me.bogeun.yajalal.service;
 
 import lombok.RequiredArgsConstructor;
 import me.bogeun.yajalal.entity.Account;
-import me.bogeun.yajalal.entity.Authority;
+import me.bogeun.yajalal.entity.Role;
 import me.bogeun.yajalal.mapper.AccountMapper;
 import me.bogeun.yajalal.payload.AccountJoinDto;
 import me.bogeun.yajalal.payload.LoginDto;
-import me.bogeun.yajalal.repository.AccountRepository;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import me.bogeun.yajalal.repository.account.AccountRepository;
+import me.bogeun.yajalal.security.utils.JwtUtils;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
-public class AccountService implements UserDetailsService {
+public class AccountService {
 
     private final AccountRepository accountRepository;
 
-    private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtils jwtUtils;
 
     private final AccountMapper accountMapper;
-
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Account account;
-
-        try {
-            account = getAccountByUsername(username);
-        } catch (IllegalArgumentException e) {
-            throw new UsernameNotFoundException("not exist username.");
-        }
-
-        return User.builder()
-                .username(account.getUsername())
-                .password(account.getPassword())
-                .roles(account.getAuthority().toString())
-                .build();
-    }
 
 
     public Account joinNewAccount(AccountJoinDto joinDto) {
         joinDto.setPassword(passwordEncoder.encode(joinDto.getPassword()));
 
         Account account = accountMapper.joinDtoToEntity(joinDto);
-        account.setAuthority(Authority.COMMON);
+        account.setRole(Role.COMMON);
 
         return accountRepository.save(account);
     }
@@ -65,21 +43,22 @@ public class AccountService implements UserDetailsService {
     }
 
     public String login(LoginDto loginDto) {
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
-        Authentication authentication;
-
-        try {
-            authentication = authenticationManager.authenticate(token);
-        } catch (AuthenticationException e) {
-            throw new BadCredentialsException("invalid login info.");
+        if (!isCorrectPassword(loginDto.getUsername(), loginDto.getPassword())) {
+            throw new IllegalArgumentException("invalid password.");
         }
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        return loginDto.getUsername();
+        return jwtUtils.generateToken(loginDto.getUsername(), Role.COMMON);
     }
 
-    public void logout() {
-        SecurityContextHolder.clearContext();
+    public boolean isCorrectPassword(String username, String password) {
+        Account account = getAccountByUsername(username);
+
+        return passwordEncoder.matches(password, account.getPassword());
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities(String username) {
+        Role role = accountRepository.getRoleByUsername(username);
+
+        return List.of(new SimpleGrantedAuthority(role.toString()));
     }
 }
