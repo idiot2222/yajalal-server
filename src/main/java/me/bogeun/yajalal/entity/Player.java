6bd0a7f38adda3983d82ae6c3bf906c2bc9c4321
idiot2222 <pky2892@gmail.com>
2022-09-20 11:51:27 +0900

    add Player Column
    선수 이름 추가

--- src/main/java/me/bogeun/yajalal/entity/Player.java
+++ src/main/java/me/bogeun/yajalal/entity/Player.java
@@ -1,9 +1,14 @@
 package me.bogeun.yajalal.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 public class Player {
 
@@ -14,6 +19,9 @@ public class Player {
     @OneToOne
     @JoinColumn(name = "account_id")
     private Account account;
+
+    @Column(name = "player_name", length = 10)
+    private String name;
 
     private Integer length;
     private Integer weight;
