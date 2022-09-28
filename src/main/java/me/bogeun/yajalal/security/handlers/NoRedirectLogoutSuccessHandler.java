06750ddf9fa4f37475a2f8087669fcf3727fdec4
idiot2222 <pky2892@gmail.com>
2022-09-28 20:47:06 +0900

    modify NoRedirectLogoutSuccessHandler
    로그아웃 성공 후 로직 변경

--- src/main/java/me/bogeun/yajalal/security/handlers/NoRedirectLogoutSuccessHandler.java
+++ src/main/java/me/bogeun/yajalal/security/handlers/NoRedirectLogoutSuccessHandler.java
@@ -1,18 +1,19 @@
 package me.bogeun.yajalal.security.handlers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 public class NoRedirectLogoutSuccessHandler implements LogoutSuccessHandler {
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        response.getWriter().write("logout success");
+        log.info("user logout");
     }
 
 }
