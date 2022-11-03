b2f036d5563373757946c0a218077b6fe9eb1c60
idiot2222 <pky2892@gmail.com>
2022-11-03 16:46:17 +0900

    add PostMapper 생성

---
+++ src/main/java/me/bogeun/yajalal/mapper/PostMapper.java
@@ -0,0 +1,10 @@
+package me.bogeun.yajalal.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PostMapper {
+
+}
+
