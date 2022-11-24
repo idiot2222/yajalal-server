4eabd6ceccce8f3b61f07495c1d851677cb8ead8
idiot2222 <pky2892@gmail.com>
2022-11-24 14:32:16 +0900

    add PostType에 TEAM_SEARCH 추가

--- src/main/java/me/bogeun/yajalal/YajalalApplication.java
+++ src/main/java/me/bogeun/yajalal/YajalalApplication.java
@@ -1,7 +1,5 @@
 package me.bogeun.yajalal;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
